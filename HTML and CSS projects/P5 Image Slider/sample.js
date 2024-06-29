b727fef419a34e82290029ad465487bd7bce4ea7
Dinesh Khichar <154075938+dineshkhichar569@users.noreply.github.com>
2024-06-30 01:03:53 +0530

    Add files via upload

---
+++ HTML and CSS projects/P5 Image Slider/sample.js
@@ -0,0 +1,24 @@
+document.querySelector(".box1").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("show1")
+})
+document.querySelector(".box2").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show2")
+})
+document.querySelector(".box3").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show3")
+})
+document.querySelector(".box4").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show4")
+})
+document.querySelector(".box5").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show5")
+})
+document.querySelector(".box6").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show6")
+})
+document.querySelector(".box7").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show7")
+})
+document.querySelector(".box8").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle("show8")
+})
